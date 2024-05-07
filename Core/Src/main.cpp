123192b29c4a68af52b5d401707bb2c4b7966faf
tai.nguyen <tai.nguyen24816@gmail.com>
2024-05-07 20:54:57 +0700

    Update code

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -18,8 +18,6 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "i2c.h"
-#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -92,8 +90,6 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
     // STF4_Rtc.init(STF4_Rtc);
