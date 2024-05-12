451e878823ba63cf5714c08efac108cb2f957d1d
tai.nguyen <tai.nguyen24816@gmail.com>
2024-05-12 21:56:48 +0700

    Update lcd spi stub

--- Drivers/LCD/LCD.hpp
+++ Drivers/LCD/LCD.hpp
@@ -3,7 +3,7 @@ class LCD : public LCD_I2c_Object
     public:
         LCD();
         ~LCD();
-    
+
     void init(void);
     void print_string(std::string string);
     LCD_TYPES::LCD_Types_enum_t get_lcd_type(void);
