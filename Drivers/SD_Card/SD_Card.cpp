d524f17e0a25bf40afd31d7bcfb2139436c47d55
tai.nguyen <tai.nguyen24816@gmail.com>
2024-05-15 23:01:32 +0700

    Update SD Object

--- Drivers/SD_Card/SD_Card.cpp
+++ Drivers/SD_Card/SD_Card.cpp
@@ -1,3 +1,42 @@
+#include <cstdint>
+#include "sdio.h"
+#include "stm32f4xx_hal_sd.h"
 #include "SD_Card.hpp"
 
+SD_Object::SD_Object()
+{
 
+}
+
+SD_Object::~SD_Object()
+{
+
+}
+
+void SD_Object::init(void)
+{
+    this->SD_status = HAL_SD_Init(&hsd);
+}
+
+void SD_Object::write(std::uint32_t address, std::uint8_t * data, std::uint32_t length)
+{
+    std::uint32_t BlockAdd = 0;
+    std::uint32_t NumberOfBlocks = 0;
+
+    if(address % 512 == 0)
+    {
+        NumberOfBlocks = (length / BLOCKSIZE) + 1;
+        BlockAdd = address / BLOCKSIZE;
+        this->SD_status = HAL_SD_WriteBlocks(&hsd, &data[0], BlockAdd, NumberOfBlocks, 65536);
+    }
+}
+
+void SD_Object::read(std::uint32_t address, std::uint8_t * Data, std::uint32_t length)
+{
+
+}
+
+void SD_Object::erase(std::uint32_t address, std::uint32_t length)
+{
+
+}
--- Drivers/SD_Card/SD_Card.hpp
+++ Drivers/SD_Card/SD_Card.hpp
@@ -3,7 +3,15 @@
 
 class SD_Object
 {
-    
+    private:
+        HAL_StatusTypeDef SD_status;
+    public:
+        SD_Object();
+        ~SD_Object();
+        void init(void);
+        void write(std::uint32_t address, std::uint8_t * Data, std::uint32_t length);
+        void read(std::uint32_t address, std::uint8_t * Data, std::uint32_t length);
+        void erase(std::uint32_t address, std::uint32_t length);
 }
 
 
